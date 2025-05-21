27c03a6f509cd142796cf3d3de451d50d7a46340
IustinianT <iusti2003@yahoo.com>
2025-05-21 16:34:17 +0100

    Add project files.

---
+++ Models/Note.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.General;
+
+namespace Blog_App.Models
+{
+    public class Note
+    {
+
+        public int Id { get; set; }
+        public string NoteTitle { get; set; }
+        public string NoteText { get; set; }
+
+        public Note() { }
+    }
+}
